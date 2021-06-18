015f4419c0b632ad811d5f293a8cf6a1628485f0
yji00 <dksro333@naver.com>
2021-06-18 11:31:55 +0900

    styled 반응형

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react'
 import Container from 'react-bootstrap/Container'
 import TitleBar from './components/TitleBar'
+import Search from './components/Search'
+import Lists from './components/Lists'
 
+
+import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/App.scss'
 
+const title = {
+	mainTitle: 'React를 배워보자',
+	subTitle: 'react-create-app / styled-components / react-bootstrap'
+}
+const design = {
+	bgColor: '#232323',
+	color: '#f9f9f9'
+}
+
 const App = () => {
-	const title = {
-		mainTitle: 'React를 배워보자',
-		subTitle: 'react-create-app / styled-components / react-bootstrap'
-	}
-	const design = {
-		bgColor: '#232323',
-		color: '#f9f9f9'
+	const [evt, setEvt] = useState([])
+	const [searchEvt, setSearchEvt] = useState([])
+	const [err, setErr] = useState(null)
+
+	useEffect(async () => {
+		try {
+
+		}
+		catch(err) {
+			setErr(err)
+		}
+		finally {
+			return () => {}
+		}
+	}, [])
+
+	const handleChange = v => {
+		// Search
 	}
 	return (
 		<Container>
 			<TitleBar title={ title } design={ design } />
+			<Search handleChange={ handleChange } />
+			<Lists />
 		</Container>
 	)
 }
